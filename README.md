8a82f79560b8c020b6d82c082bd9f371f3c84107
kimsooyoung <tge1375@naver.com>
2024-07-07 15:20:06 +0900

    franka till 3f

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # isaac_ros2_joint_examples
+
+```
+ros2 run franka_basic_ros2_bridge franka_ros2_publisher 
+ros2 run franka_2f_ros2_bridge franka_ros2_publisher 
+ros2 run franka_3f_ros2_bridge franka_ros2_publisher 
+
+```
---
+++ franka_3f_ros2_bridge/franka_3f_ros2_bridge/franka_ros2_publisher.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import JointState
+import numpy as np
+import time
+
+
+class TestROS2Bridge(Node):
+    def __init__(self):
+
+        super().__init__("franka_ros2_bridge")
+
+        # Create the publisher. This publisher will publish a JointState message to the /joint_command topic.
+        self.publisher_ = self.create_publisher(JointState, "/issac/joint_command", 10)
+
+        # Create a JointState message
+        self.joint_state = JointState()
+
+        self.joint_state.name = [
+            "panda_joint1",
+            "panda_joint2",
+            "panda_joint3",
+            "panda_joint4",
+            "panda_joint5",
+            "panda_joint6",
+            "panda_joint7",
+            "palm_finger_1_joint",
+            "palm_finger_2_joint",
+            "finger_2_joint_1",
+            "finger_2_joint_2",
+            "finger_2_joint_3",
+            "finger_1_joint_1", 
+            "finger_1_joint_2",
+            "finger_1_joint_3",
+            "finger_middle_joint_1",
+            "finger_middle_joint_2",
+            "finger_middle_joint_3",
+        ]
+
+        num_joints = len(self.joint_state.name)
+
+        # make sure kit's editor is playing for receiving messages
+        self.joint_state.position = np.array([0.0] * num_joints, dtype=np.float64).tolist()
+        self.default_joints = [
+            0.0, -1.16, -0.0, -2.3, -0.0, 1.6, 1.1, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ]
+
+        # limiting the movements to a smaller range (this is not the range of the robot, just the range of the movement
+        self.max_joints = np.array([
+            0.5, -0.66, 0.5, -1.8, 0.5, 1.7, 1.6, 10.0, 10.0,
+            70.0, 90.0, 0.0, 70.0, 90.0, 0.0, 70.0, 90.0, 0.0
+        ])
+        self.min_joints = np.array([
+            -0.5, -1.66, -0.5, -2.8, -0.5, 1.1, 0.6, -10.0, -10.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ])
+
+        # position control the robot to wiggle around each joint
+        self.time_start = time.time()
+
+        timer_period = 0.05  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        self.joint_state.header.stamp = self.get_clock().now().to_msg()
+
+        joint_position = (
+            np.sin(time.time() - self.time_start) * (self.max_joints - self.min_joints) * 0.5 + self.default_joints
+        )
+        self.joint_state.position = joint_position.tolist()
+
+        # Publish the message to the topic
+        self.publisher_.publish(self.joint_state)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    ros2_publisher = TestROS2Bridge()
+
+    rclpy.spin(ros2_publisher)
+
+    # Destroy the node explicitly
+    ros2_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
